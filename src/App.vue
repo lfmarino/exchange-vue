4741392d7f0c3f209a4ac6909aab99cb3413f18b
Luis Felipe Marin Orrego <felipe.marin@tisatechnology.com>
2019-11-27 16:33:52 -0500

    Fetching data from API

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main>
+    <px-header />
+    <router-view class="container px-5 sm:px-20 py-20 flex justify-center" />
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import PxHeader from '@/components/PxHeader'
 export default {
-  name: "app",
+  name: 'app',
   components: {
-    HelloWorld
+    PxHeader
   }
-};
+}
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/PxAssetsTable.vue
@@ -0,0 +1,87 @@
+<template>
+  <table>
+    <thead>
+      <tr class="bg-gray-100 border-b-2 border-gray-400">
+        <th></th>
+        <th>
+          <span>Ranking</span>
+        </th>
+        <th>Nombre</th>
+        <th>Precio</th>
+        <th>Cap. de Mercado</th>
+        <th>Variación 24hs</th>
+        <td class="hidden sm:block"></td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="asset in assets"
+        v-bind:key="asset.id"
+        class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100"
+      >
+        <td>
+          <img
+            v-bind:src="
+              `https://static.coincap.io/assets/icons/${asset.symbol.toLowerCase()}@2x.png`
+            "
+            v-bind:alt="asset"
+          />
+        </td>
+        <td>
+          <b># {{ asset.rank }}</b>
+        </td>
+        <td>{{ asset.name }}</td>
+        <td>{{ asset.priceUsd }}</td>
+        <td>{{ asset.marketCapUsd }}</td>
+        <td>{{ asset.changePercent24Hr }}</td>
+        <td class="hidden sm:block"></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'PxAssetsTable',
+
+  props: {
+    assets: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style scoped>
+.up::before {
+  content: '👆';
+}
+
+.down::before {
+  content: '👇';
+}
+
+td {
+  padding: 20px 0px;
+  font-size: 0.6rem;
+  text-align: center;
+}
+
+th {
+  padding: 5px;
+  font-size: 0.6rem;
+}
+
+@media (min-width: 640px) {
+  td,
+  th {
+    padding: 20px;
+    font-size: 1rem;
+  }
+
+  th {
+    padding: 12px;
+  }
+}
+</style>
